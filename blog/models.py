0bd23d1a1fc643be3d5fab647345ee0bfd2c390f
Xuan Truong Luu <xuantruong1234@gmail.com>
2021-05-09 16:42:36 +0700

    Add user api

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, String, Integer
+from .database import Base
+
+class Blog(Base):
+    __tablename__ ="blog"
+    id = Column(Integer, primary_key=True, index= True)
+    title = Column(String)
+    body = Column(String)
+
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True, index= True)
+    name = Column(String)
+    email = Column(String)
+    password=Column(String)
---
+++ blog/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    title:str
+    body:str
+
+class ShowBlog(BaseModel):
+    title:str
+    class Config():
+        orm_mode=True
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    class Config():
+        orm_mode=True
